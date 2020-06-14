fdc0e449ee7497b891d5ffb14e300bec6f64487b
Daniel Casadevall Pino <dpino@nianticlabs.com>
2020-06-13 20:12:45 -0700

    Ask to play again

--- Section_02/BullCowGame/Main.cpp
+++ Section_02/BullCowGame/Main.cpp
@@ -18,19 +18,22 @@ void PlayGame();
 std::string GetGuess();
 
 /**
+ * Asks the player if they want to play again.
+ * @return True if the player said yes. False otherwise.
+ */
+bool AskToPlayAgain();
+
+/**
  * Application entry point
  */
 int main() {
     PrintIntro();
-    PlayGame();
+
+    do {
+        PlayGame();
+    } while (AskToPlayAgain());
 
     return 0;
-}
-void PlayGame() {
-    constexpr int NUMBER_OF_TURNS = 5;
-    for (int i = 0; i < NUMBER_OF_TURNS; i++) {
-        std::cout << "Your guess was: " << GetGuess() << std::endl;
-    }
 }
 
 void PrintIntro() {
@@ -40,10 +43,26 @@ void PrintIntro() {
     std::cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 }
 
+void PlayGame() {
+    constexpr int NUMBER_OF_TURNS = 5;
+    for (int i = 0; i < NUMBER_OF_TURNS; i++) {
+        std::string Guess = GetGuess();
+        std::cout << "Your guess was: " << Guess << std::endl;
+    }
+}
+
 std::string GetGuess() {
     std::cout << "Enter your guess: ";
     std::string Guess;
     std::getline(std::cin, Guess);
 
     return Guess;
+}
+
+bool AskToPlayAgain() {
+    std::string Response;
+    std::cout << "Do you want to play again (y/n)? ";
+    std::getline(std::cin, Response);
+
+    return Response.length() > 0 && (Response[0] == 'y' || Response[0] == 'Y');
 }
